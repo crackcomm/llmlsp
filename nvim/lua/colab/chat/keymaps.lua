40e688447b8a31238688864743739492eca3f76f
Łukasz Kurowski <crackcomm@gmail.com>
2024-07-21 20:29:15 +0200

    docs(nvim/keymaps): add description to few keymaps

--- nvim/lua/colab/chat/keymaps.lua
+++ nvim/lua/colab/chat/keymaps.lua
@@ -7,22 +7,22 @@ function M.on_attach(_, bufnr)
   vim.keymap.set("v", "<leader>cf", function()
     local sel = utils.get_selection()
     chat_commands.float(bufnr, sel.start_line, sel.end_line)
-  end, { buffer = bufnr })
+  end, { buffer = bufnr, desc = "LLMLSP: [C]omplete [F]ragment" })
 
-  vim.keymap.set("v", "<leader>ar", function()
+  vim.keymap.set("v", "<leader>af", function()
     local sel = utils.get_selection()
     chat_commands.add_contents(bufnr, sel.start_line, sel.end_line)
-  end, { buffer = bufnr })
+  end, { buffer = bufnr, desc = "LLMLSP: [A]ppend [F]ragment" })
 end
 
 function M.on_init()
   vim.keymap.set("n", "<leader>cc", function()
     chat_commands.toggle()
-  end)
+  end, { desc = "LLMLSP: [C]hat [C]ommands" })
 
   vim.keymap.set("n", "<leader>ch", function()
     chat_commands.history()
-  end)
+  end, { desc = "LLMLSP: [C]hat [H]istory" })
 end
 
 return M
